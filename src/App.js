ffae4bcb6289bab8f8939425d465db589f97893c
vikrantkkk <120943926+vikrantkkk@users.noreply.github.com>
2023-05-19 17:25:17 +0530

    css files modified

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
   ];
   return (
     <div>
-      <Select options={options} isSearchable={true} />
+      <Select options={options} isSearchable={true} className="Select" />
     </div>
   );
 }
