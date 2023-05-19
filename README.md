9ab43e192b786800c2d4d0af551ba69b256d0dd2
vikrantkkk <120943926+vikrantkkk@users.noreply.github.com>
2023-05-19 17:21:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Make-a-Searchable-Dropdown
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-2k8rxh)
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+import Select from 'react-select';
+
+export default function App() {
+  const options = [
+    { value: 'mango', label: 'Mango' },
+    { value: 'apple', label: 'Apple' },
+    { value: 'banana', label: 'Banana' },
+    { value: 'orange', label: 'Orange' },
+  ];
+  return (
+    <div>
+      <Select options={options} isSearchable={true} />
+    </div>
+  );
+}
